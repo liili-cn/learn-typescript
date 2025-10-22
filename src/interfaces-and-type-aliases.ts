3866ceaaf2456db442c135bdb8cbc8a189711c82
liili <liili_cn@hotmail.com>
2025-10-22 16:15:40 +0800

    学习接口和类型别名

---
+++ src/interfaces-and-type-aliases.ts
@@ -0,0 +1,110 @@
+// 类型别名
+type Amount = {
+  currency: string;
+  value: number;
+};
+
+function frintAmount(amt: Amount) {
+  console.log(amt);
+  const { currency, value } = amt;
+  console.log(`${currency} ${value}`);
+}
+
+// 可以在同一作用域中，只能声明一次
+// type Amount = { // 报错
+//   fail: "this will not work";
+// };
+
+// 类型别名中的继承（使用交叉类型）
+type SpecialDate = Date & { getDescription(): string };
+
+const newYearsEve: SpecialDate = Object.assign(new Date(), {
+  getDescription: () => "Last day of year",
+});
+
+// 接口
+interface Amount2 {
+  currency: string;
+  value: number;
+}
+
+function frintAmounts(amt: Amount2) {
+  console.log(`${amt.currency} ${amt.value}`);
+}
+
+// 接口中的继承（extends）
+// 子类继承父类
+function consumeFood(arg) {}
+class AnimalThatEats {
+  eat(food) {
+    consumeFood(food);
+  }
+}
+class Cat extends AnimalThatEats {
+  meow() {
+    return "meow";
+  }
+}
+const c = new Cat();
+c.eat;
+// ^?
+c.meow();
+//  ^?
+
+// 子接口继承父接口
+interface Animal {
+  isAlive(): boolean;
+}
+
+interface Mammal extends Animal {
+  getFurOrHairColor(): string;
+}
+
+interface Hamster extends Mammal {
+  squeak(): string;
+}
+
+function careForHamster(h: Hamster) {
+  h.getFurOrHairColor();
+  h.squeak();
+}
+
+// 接口中的继承（implements）
+interface AnimalLike {
+  eat(food): void;
+}
+
+class Dog implements AnimalLike {
+  bark() {
+    return "woof";
+  }
+  // 添加 eat 方法，否则 Dog 类不符合 AnimalLike 接口
+  eat(food) {
+    consumeFood(food);
+  }
+}
+
+// 开放接口
+interface AnimalLike {
+  eat(food): void;
+}
+
+function feed(animal: AnimalLike) {
+  animal.eat;
+  animal.isAlive;
+}
+
+interface AnimalLike {
+  isAlive(): boolean;
+}
+
+// 递归
+type NestedNumbers = number | NestedNumbers[];
+
+const val: NestedNumbers = [3, 4, [5, 6, [7], 59], 221];
+
+if (typeof val !== "number") {
+  val.push(41);
+
+  val.push("this will not work"); // 报错
+}
